4443109b2dbaf6a720bd2f524a14312559282193
Copser <ppilipovic84@gmail.com>
2019-06-27 13:35:02 +0200

    master
    - add gitignore
    - check indent in popup.js

--- popup.js
+++ popup.js
@@ -2,8 +2,8 @@
 
 var setDOMInfo = info => {
 	document.getElementById('url').textContent = info.url,
-  document.getElementById('total').textContent = info.total,
-  document.getElementById('tags').textContent = info.tags;
+	document.getElementById('total').textContent = info.total,
+	document.getElementById('tags').textContent = info.tags;
 }
 
 
@@ -11,10 +11,9 @@ window.addEventListener("DOMContentLoaded", () => {
 	chrome.tabs.query({
 		active: true,
 		currentWindow: true
-	}, tabs => {
+		}, tabs => {
 		chrome.tabs.sendMessage(
 			tabs[0].id,
-			{from: 'popup', subject: 'DOMInfo'},
-			setDOMInfo);
+			{from: 'popup', subject: 'DOMInfo'}, setDOMInfo);
 	});
 });
